6b3eff9881cbbf42c5e9b18de6f5b8c634c8bf43
naitik256 <rajnaitik620@gmail.com>
2025-05-21 12:12:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,40 @@
-// sw.js
 const CACHE_NAME = 'study-stopwatch-cache-v1';
 const urlsToCache = [
-    '/',             
-    '/index.html',  
-    '/styles.css',
-    '/script.js',
-    '/face-api.min.js',
-    // ...cache paths to model files in the models directory...
-    '/models/tiny_face_detector_model-shard1',
-    '/models/tiny_face_detector_model-weights_manifest.json',
-    '/models/face_landmark_68_model-shard1',
-    // ... and so on for other model files
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/face-api.min.js',
+  '/models/tiny_face_detector_model-shard1',
+  '/models/tiny_face_detector_model-weights_manifest.json',
+  '/models/face_landmark_68_model-shard1',
+  '/models/face_landmark_68_model-weights_manifest.json'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
